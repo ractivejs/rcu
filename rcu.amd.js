b3c20f48653876e271066e5901992b0c902a3c4c
Rich Harris <richard.a.harris@gmail.com>
2014-07-05 17:42:17 -0400

    rebuild

--- rcu.amd.js
+++ rcu.amd.js
@@ -1,6 +1,6 @@
 /*
 
-	rcu (Ractive component utils) - 0.1.8 - 2014-06-20
+	rcu (Ractive component utils) - 0.2.0 - 2014-07-05
 	==============================================================
 
 	Copyright 2014 Rich Harris and contributors
@@ -29,29 +29,33 @@ define( function() {
 
 		var requirePattern = /require\s*\(\s*(?:"([^"]+)"|'([^']+)')\s*\)/g;
 		return function parse( source ) {
-			var template, links, imports, scripts, script, styles, match, modules, i, item;
-			template = Ractive.parse( source, {
+			var parsed, template, links, imports, scripts, script, styles, match, modules, i, item;
+			parsed = Ractive.parse( source, {
 				noStringify: true,
 				interpolateScripts: false,
 				interpolateStyles: false
 			} );
+			if ( parsed.v !== 1 ) {
+				throw new Error( 'Mismatched template version! Please ensure you are using the latest version of Ractive.js in your build process as well as in your app' );
+			}
 			links = [];
 			scripts = [];
 			styles = [];
 			modules = [];
 			// Extract certain top-level nodes from the template. We work backwards
 			// so that we can easily splice them out as we go
+			template = parsed.t;
 			i = template.length;
 			while ( i-- ) {
 				item = template[ i ];
 				if ( item && item.t === 7 ) {
-					if ( item.e === 'link' && ( item.a && item.a.rel[ 0 ] === 'ractive' ) ) {
+					if ( item.e === 'link' && ( item.a && item.a.rel === 'ractive' ) ) {
 						links.push( template.splice( i, 1 )[ 0 ] );
 					}
-					if ( item.e === 'script' && ( !item.a || !item.a.type || item.a.type[ 0 ] === 'text/javascript' ) ) {
+					if ( item.e === 'script' && ( !item.a || !item.a.type || item.a.type === 'text/javascript' ) ) {
 						scripts.push( template.splice( i, 1 )[ 0 ] );
 					}
-					if ( item.e === 'style' && ( !item.a || !item.a.type || item.a.type[ 0 ] === 'text/css' ) ) {
+					if ( item.e === 'style' && ( !item.a || !item.a.type || item.a.type === 'text/css' ) ) {
 						styles.push( template.splice( i, 1 )[ 0 ] );
 					}
 				}
@@ -68,8 +72,8 @@ define( function() {
 			// Extract names from links
 			imports = links.map( function( link ) {
 				var href, name;
-				href = link.a.href && link.a.href[ 0 ];
-				name = link.a.name && link.a.name[ 0 ] || getName( href );
+				href = link.a.href;
+				name = link.a.name || getName( href );
 				if ( typeof name !== 'string' ) {
 					throw new Error( 'Error parsing link tag' );
 				}
@@ -84,7 +88,7 @@ define( function() {
 			}
 			// TODO glue together text nodes, where applicable
 			return {
-				template: template,
+				template: parsed,
 				imports: imports,
 				script: script,
 				css: styles.map( extractFragment ).join( ' ' ),
@@ -188,6 +192,7 @@ define( function() {
 				var options, Component, script, factory, component, exports, prop;
 				options = {
 					template: definition.template,
+					partials: definition.partials,
 					css: definition.css,
 					components: imports
 				};
