e6bbdd4d1ec0835348d4b9b7adb610877c81f113
Rich Harris <richard.a.harris@gmail.com>
2014-09-28 22:39:42 -0400

    move jshint config to .jshintrc

--- grunt/config/jshint.js
+++ grunt/config/jshint.js
@@ -1,27 +1,6 @@
 module.exports = {
 	files: [ 'src/**/*.js' ],
 	options: {
-		esnext: true,
-		boss: true,
-		eqnull: true,
-		evil: true,
-		laxbreak: true,
-		proto: true,
-		smarttabs: true,
-		strict: false,
-		undef: true,
-		unused: true,
-		'-W018': true,
-		'-W041': false,
-		globals: {
-			define: true,
-			module: true,
-			require: true,
-			setInterval: true,
-			setTimeout: true,
-			Ractive: true,
-			document: true,
-			window: true
-		}
+		jshintrc: '.jshintrc'
 	}
 };
