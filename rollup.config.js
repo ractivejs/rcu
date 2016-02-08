281e03fda681b92adeb52637f8d38253aa05137c
Rich-Harris <richard.a.harris@gmail.com>
2016-02-08 17:01:04 -0500

    rollup-plugin-npm is now rollup-plugin-node-resolve

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
-import npm from 'rollup-plugin-npm';
+import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 
 export default {
 	entry: 'src/rcu.js',
 	plugins: [
-		npm({ jsnext: true }),
+		nodeResolve({ jsnext: true }),
 		babel()
 	],
 	moduleName: 'rcu'
