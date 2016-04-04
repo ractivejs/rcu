ef8e0673140abd89c4d75c8090cbd30492665212
Rich-Harris <richard.a.harris@gmail.com>
2016-04-04 08:58:15 -0400

    tidy up build a bit

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,19 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import modifyBabelPreset from 'modify-babel-preset';
 
 export default {
 	entry: 'src/rcu.js',
 	plugins: [
 		nodeResolve({ jsnext: true }),
-		babel()
+		babel({
+			presets: [ modifyBabelPreset( 'es2015', {
+				'transform-typeof-symbol': false,
+				'transform-modules-commonjs': false
+			})],
+			babelrc: false,
+			exclude: 'node_modules/**'
+		})
 	],
 	moduleName: 'rcu'
 };
